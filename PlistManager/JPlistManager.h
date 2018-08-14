e476b722c140f3d8523134d35bcc96c3fc0393aa
jiangqi941017 <jiangqi@MacBook-Pro.local>
2018-08-14 13:22:46 +0800

    单例

--- PlistManager/JPlistManager.h
+++ PlistManager/JPlistManager.h
@@ -10,15 +10,19 @@
 
 @interface JPlistManager : NSObject
 
++(instancetype)alloc __attribute__((unavailable("call shareInstance instead")));
 ///禁止 new
-+(instancetype) new __attribute__((unavailable("call alloc init instead")));
++(instancetype) new __attribute__((unavailable("call shareInstance instead")));
+
+@property (class, readonly, strong) JPlistManager *shareInstance;
 
 /**
-操作plist
-
+ 操作plist
+ 
  @param pathBlock  设置路径
  @param handleBlock 数据操作
  @return 这个闭包是用来存储更新到本地，如果这是查询不需要更新本地，可以不调用这个返回值
  */
 -(void(^)(void))nativeConfigOperate:(NSArray<NSString *>*(^)(void))pathBlock handle:(void(^)(NSMutableDictionary *valueLastDic, id value))handleBlock;
 @end
+
